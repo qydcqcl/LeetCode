3e06f1b3a74d23eed59d184579eb58738a472487
qydcqcl <ltt20150220>
2020-04-27 11:07:14 +0800

    update java14demo

---
+++ src/test/java/Java14Test.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+/**
+ * @author hzq
+ * @date 2020/4/27 11:05
+ */
+public class Java14Test {
+
+//    @Test
+//    public void test1(){
+//        String day = "1";
+//
+//        switch (day){
+//            case "1" -> System.out.println("as");
+//            case "2" -> System.out.println("df");
+//            default -> {
+//                System.out.println("sdf");
+//            }
+//        }
+//    }
+//
+//    @Test
+//    public void test2(){
+//        Object obj = "hello world";
+//        if(obj instanceof String str){
+//            System.out.println(str);
+//        }
+//    }
+//
+//    @Test
+//    public void test3(){
+//        String s = """
+//                <html>
+//                    <body>
+//                        <p>hello world</p>
+//                    </body>
+//                </html>
+//                """;
+//        System.out.println(s);
+//    }
+}
