c09fce6fa4b7fb8eaae733b3e98a0739928c017d
huxiaoming <huxiaoming@gmail.com>
2020-05-02 17:17:24 +0800

    add demo160 167 168 169

---
+++ src/main/java/org/example/simple/demo168/Example1.java
@@ -0,0 +1,54 @@
+package org.example.simple.demo168;
+
+/**
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如，
+ *
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB
+ *     ...
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "A"
+ * 示例 2:
+ *
+ * 输入: 28
+ * 输出: "AB"
+ * 示例 3:
+ *
+ * 输入: 701
+ * 输出: "ZY"
+ *
+ * @author hzq
+ * @date 2020/5/2 16:05
+ */
+public class Example1 {
+
+    public static void main(String[] args) {
+        Example1 example1 = new Example1();
+        int n = 28;
+        String s = example1.convertToTitle(n);
+        System.out.println(s);
+    }
+
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n > 0){
+            int c = n % 26;
+            if(c == 0){
+                c = 26;
+                n -= 1;
+            }
+            sb.insert(0, (char) ('A' + c - 1));
+            n /= 26;
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/example/simple/demo169/Example1.java
@@ -0,0 +1,53 @@
+package org.example.simple.demo169;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ *
+ * @author hzq
+ * @date 2020/5/2 16:25
+ */
+public class Example1 {
+
+    public static void main(String[] args) {
+        Example1 example1 = new Example1();
+        int[] nums = {2,2,1,1,1,2,2};
+        int i = example1.majorityElement(nums);
+        System.out.println(i);
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if(majorityEntry == null || entry.getValue() > majorityEntry.getValue() ){
+                majorityEntry = entry;
+            }
+        }
+        return majorityEntry.getKey();
+    }
+
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if(!counts.containsKey(num)){
+                counts.put(num, 1);
+            }else{
+                counts.put(num, counts.get(num) + 1);
+            }
+        }
+        return counts;
+    }
+}
