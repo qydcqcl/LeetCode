94dbe040318454abb8e8c7eb33fa2c7249f3188b
xiaoming <35033187+qydcqcl@users.noreply.github.com>
2020-06-05 17:31:10 +0800

    add demo226

---
+++ src/main/java/org/example/simple/demo226/TreeNode.java
@@ -0,0 +1,12 @@
+package org.example.simple.demo226;
+
+/**
+ * @author hzq
+ * @date 2020/6/5 16:47
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
